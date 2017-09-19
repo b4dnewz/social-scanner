2b5830e871db67af37f62e8dac5a24976f706635
b4dnewz <filippo@codekraft.it>
2017-09-19 21:51:11 +0200

    First commit

---
+++ example/index.js
@@ -0,0 +1,8 @@
+const socialScanner = require('../lib/index');
+
+socialScanner('codekraft-studio', null, (err, response) => {
+  if (err) {
+    console.log('Error:', err);
+  }
+  console.log('Response:', response);
+});
