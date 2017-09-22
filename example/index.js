0366742138c0c98a07a4c7a6e043839268341a4c
b4dnewz <filippo@codekraft.it>
2017-09-22 14:02:13 +0200

    Added support for restrict option

--- example/index.js
+++ example/index.js
@@ -1,8 +1,12 @@
 const socialScanner = require('../lib/index');
 
-socialScanner('codekraft-studio', null, {}, (err, response) => {
+socialScanner('codekraft-studio', {
+  restrict: ['github', 'bitbucket', 'polyvore'],
+  screenshotOptions: {}
+}, (err, response) => {
   if (err) {
     console.log('Error:', err);
+    return;
   }
   console.log('Response:', response);
 });
